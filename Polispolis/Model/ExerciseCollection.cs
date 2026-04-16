f6cdde0613a112553252aabab0651476afc81331
Emil Berg <emil94berg@gmail.com>
2026-04-16 10:42:19 +0200

    New model för adding Exercises to a Session

---
+++ Polispolis/Model/ExerciseCollection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polispolis.Model
+{
+    public class ExerciseCollection
+    {
+        public Category Category { get; set; }
+        public Exercise Exercise { get; set; }
+    }
+}
